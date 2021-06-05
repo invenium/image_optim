2700085dd66d4467ed999fbad391d1364eeb581b
Ivan Kuchin <tadump+git@gmail.com>
2021-06-05 15:00:44 +0200

    correct oxipng

--- lib/image_optim/worker/oxipng.rb
+++ lib/image_optim/worker/oxipng.rb
@@ -30,18 +30,19 @@ class ImageOptim
         -4
       end
 
-      def optimize(src, dst)
+      def optimize(src, dst, options = {})
         src.copy(dst)
         args = %W[
           -o #{level}
           --quiet
+          --
           #{dst}
         ]
         args.unshift "-i#{interlace ? 1 : 0}" unless interlace.nil?
         if strip
           args.unshift '--strip', 'all'
         end
-        execute(:oxipng, *args) && optimized?(src, dst)
+        execute(:oxipng, args, options) && optimized?(src, dst)
       end
 
       def optimized?(src, dst)
